a7e1e9a75bde5a1acf288ebc3a6c4228e1586a48
SkyQueenSE <Jackiie.samuelsson@gmail.com>
2025-05-17 20:28:13 +0200

    Add mock secrets file

---
+++ ESP8266Lightswitch_WiFiserver/mock_secrets.h
@@ -0,0 +1,8 @@
+#ifndef STASSID
+#define STASSID "YOUR_SSID"
+#define STAPSK "YOUR_SSID_PASSWORD"
+#endif
+
+const char *ssid = STASSID;
+const char *password = STAPSK;
+const char *host = "LOCAL_IP_OF_ESP8266";
